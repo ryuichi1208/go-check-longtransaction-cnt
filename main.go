6b172cb421fe7574ea8011a7ea54a91a35b1da6d
Ryuichi Watanabe <ryucrosskey@gmail.com>
2022-11-06 21:03:43 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/ryuichi1208/go-check-longtransaction-cnt/cmd"
 )
+
+func init() {
+	if os.Getenv("MYSQL_PASSWORD") == "" {
+		fmt.Println("Environment variable MYSQL_PASSWORD not set")
+		os.Exit(1)
+	}
+}
 
 func main() {
 	cmd.Run()
